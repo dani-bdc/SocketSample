c7ed4e7c609144fd28a2f862178a56d28151e8db
dmart126 <dmart126@DESKTOP-HMA8JE5>
2023-10-19 19:49:28 +0200

    Add project files.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        static void ExecuteClient()
+        {
+            int port = 21000;
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            Thread.Sleep(20000);
+            try
+            {
+                Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(endPoint);
+                if (sender.RemoteEndPoint != null)
+                {
+                    Console.WriteLine("Socket client connected to: " + sender.RemoteEndPoint.ToString());
+                }
+
+                byte[] message = Encoding.UTF8.GetBytes("Test Client<EOF>");
+                int byteSend = sender.Send(message);
+
+                byte[] messageReceived = new byte[byteSend];
+                int byteReceived = sender.Receive(messageReceived);
+
+                Console.WriteLine("Message from server: {0}", Encoding.UTF8.GetString(messageReceived, 0, byteReceived));
+                
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            } 
+            catch (ArgumentNullException ane)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StartServer();
+    }
+
+    private static void StartServer()
+    {
+        int port = 21000;
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ip = host.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+        Thread.Sleep(10000);
+        try
+        {
+            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(endPoint);
+            listener.Listen();
+            while (true)
+            {
+                Console.WriteLine("Waiting for connection");
+                Socket clientSocket = listener.Accept();
+
+                byte[] bytes = new byte[4096];
+                string data = "";
+                while (true)
+                {
+                    int numBytes = clientSocket.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, numBytes);
+                    if (data.IndexOf("<EOF>") > -1)
+                        break;
+                }
+
+                Console.WriteLine("Text received: {0}", data);
+
+                byte[] message = Encoding.UTF8.GetBytes("Message from server");
+                
+                clientSocket.Send(message);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
